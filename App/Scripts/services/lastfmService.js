bbfa3dcafb13fc3696272c70674b9096dc8957b5
evan-ward <evan.ward1@gmail.com>
2014-09-29 23:11:13 -0600

    updates

--- App/Scripts/services/lastfmService.js
+++ App/Scripts/services/lastfmService.js
@@ -1,19 +1,18 @@
 var app = angular.module('mapperApp');
 
-app.service('lastfmService', function($http, $q){
+app.service('lastfmService', function($http, $q) {
 
 	var deferred = $q.defer();
 
-	var searchLocations = function(myLocation) {
+	var getLocationEvents = function(myLocation) {
 
 		if (myLocation) {
 		var myLocationUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location="+myLocation+"&api_key=c172959585e074ab3a45cf3496bd24f3&format=json";
 
-			return $http.get(myLocationUrl).then(function(res){
+			return $http.get(myLocationUrl).then(function(res) {
 				console.log(res.data.events.event);
 				return res.data.events.event;
 			})
-
 		}
 	}
 
@@ -22,21 +21,17 @@ app.service('lastfmService', function($http, $q){
 		if (artist) {
 		var simArtistUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist+"&api_key=c172959585e074ab3a45cf3496bd24f3&format=json";
 
-			return $http.get(simArtistUrl).then(function(res){
+			return $http.get(simArtistUrl).then(function(res) {
 				console.log(res);
 				return res.data.similarartists.artist;
 			})
-
 		}
 	}
 
-	this.search = function(artist, myLocation){
+	this.search = function(artist, myLocation) {
 		
-		searchLocations(myLocation).then(function(res){
-			searchSimilarArtists(artist);
-		});
+		getLocationEvents(myLocation);
+		searchSimilarArtists(artist);
 
-
-
-	};
+		};
 });
